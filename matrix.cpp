c06e2e4adf7732fb38746f619e9022964626d99b
ibenehasit <ibeneessem@hotmail.com>
2025-11-03 12:51:41 +0000

    simple class usage for matrix backbone

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+
+class Matrix{
+    int Row;
+    int Col;
+
+public:
+    Matrix(int row, int col){
+        Row = row;
+        Col = col;
+    }
+
+    int getRow(){
+        return Row;
+    }
+
+    int getCol(){
+        return Col;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, world!";
+    Matrix matrix1(6, 7);
+    std::cout << matrix1.getRow() << "\n";
+    std::cout << matrix1.getCol() << "\n";
     return 0;
 }
