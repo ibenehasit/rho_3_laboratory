c53ed8f0e97340ed766d26ebbab168445cf75982
ibenehasit <ibeneessem@hotmail.com>
2025-11-04 13:34:30 +0000

    Matrix multiplication functionality added

--- matrix.cpp
+++ matrix.cpp
@@ -13,12 +13,30 @@ public:
         Row = row;
         Col = col;
 
-        data.resize(Row); // Create 'Row' number of empty vectors
+        data.resize(Row); 
         for (int i = 0; i < Row; i++) {
             data[i].resize(Col, 0.0); 
-    }
+        }
     }
 
+    //function overload baybee
+    Matrix operator*(const Matrix& other) const {
+        if (Col != other.Row) {
+            std::cerr << "Matrix dimensions incompatible for multiplication\n";
+            return Matrix(0, 0); // Return empty matrix as error indicator
+        }
+
+        Matrix result(Row, other.Col); // this->Row and other.Col
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < other.Col; j++) {
+                result.data[i][j] = 0;
+                for (int k = 0; k < Col; k++) { // this->Col
+                    result.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
     
     int getRow(){
         return Row;
@@ -36,12 +54,18 @@ public:
             std::cout << "\n";
         }
     }
-
 };
 
 int main() {
     Matrix matrix1(6, 7);
     std::cout << "This is a " << matrix1.getRow() << "x" << matrix1.getCol() << " matrix\n";
     matrix1.print();
+    
+    // Test multiplication
+    Matrix matrix2(7, 3); // Compatible dimensions: 6x7 * 7x3 = 6x3
+    Matrix result = matrix1 * matrix2;
+    std::cout << "\nMultiplication result is a " << result.getRow() << "x" << result.getCol() << " matrix\n";
+    result.print();
+    
     return 0;
 }
