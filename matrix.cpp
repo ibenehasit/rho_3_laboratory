0eddfee412a3f24fc3fbcb75efb490bfd444a392
ibenehasit <ibeneessem@hotmail.com>
2025-11-04 15:49:13 +0000

    Added + and * functionality for matrices

--- matrix.cpp
+++ matrix.cpp
@@ -19,18 +19,17 @@ public:
         }
     }
 
-    //function overload baybee
     Matrix operator*(const Matrix& other) const {
         if (Col != other.Row) {
             std::cerr << "Matrix dimensions incompatible for multiplication\n";
-            return Matrix(0, 0); // Return empty matrix as error indicator
+            return Matrix(0, 0);
         }
 
-        Matrix result(Row, other.Col); // this->Row and other.Col
+        Matrix result(Row, other.Col);
         for (int i = 0; i < Row; i++) {
             for (int j = 0; j < other.Col; j++) {
                 result.data[i][j] = 0;
-                for (int k = 0; k < Col; k++) { // this->Col
+                for (int k = 0; k < Col; k++) {
                     result.data[i][j] += data[i][k] * other.data[k][j];
                 }
             }
@@ -39,14 +38,31 @@ public:
     }
     
     Matrix operator*(double scalar) const {
-    Matrix result(Row, Col);
-    for (int i = 0; i < Row; i++) {
-        for (int j = 0; j < Col; j++) {
-            result.data[i][j] = data[i][j] * scalar;
+        Matrix result(Row, Col);
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < Col; j++) {
+                result.data[i][j] = data[i][j] * scalar;
+            }
         }
+        return result;
     }
-    return result;
-}
+
+    Matrix operator+(const Matrix& other) const {
+        if (Row != other.Row || Col != other.Col) {
+            std::cerr << "Error: Matrices must have same dimensions for addition\n";
+            return Matrix(0, 0);
+        }
+
+        Matrix result(Row, Col);
+
+        for(int i = 0; i < Row; i++){
+            for (int j = 0; j < Col; j++) {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
     int getRow(){
         return Row;
     }
@@ -70,15 +86,19 @@ int main() {
     std::cout << "This is a " << matrix1.getRow() << "x" << matrix1.getCol() << " matrix\n";
     matrix1.print();
     
-    // Test matrix multiplication
-    Matrix matrix2(7, 3); // Compatible dimensions: 6x7 * 7x3 = 6x3
+    Matrix matrix2(7, 3);
     Matrix result1 = matrix1 * matrix2;
     std::cout << "\nMultiplication result is a " << result1.getRow() << "x" << result1.getCol() << " matrix\n";
     result1.print();
     
-    //Test scalar-matrix multiplication
     Matrix result2 = matrix1 * 5;
-    std::cout << "\nMultiplication result is a " << result2.getRow() << "x" << result2.getCol() << " matrix\n";
+    std::cout << "\nScalar multiplication result is a " << result2.getRow() << "x" << result2.getCol() << " matrix\n";
     result2.print();
+    
+    Matrix matrix3(6,7);
+    std::cout << "\nAddition result is a " << matrix3.getRow() << "x" << matrix3.getCol() << " matrix\n";
+    Matrix result3 = matrix1 + matrix3;
+    result3.print();
+    
     return 0;
 }
