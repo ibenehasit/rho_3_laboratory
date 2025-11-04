1641b0bfa4099aeddcb2b4acca570acb1e227074
ibenehasit <ibeneessem@hotmail.com>
2025-11-04 12:59:21 +0000

    print funcitonality added

--- matrix.cpp
+++ matrix.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <vector>
 
+using std::vector;
+
 class Matrix{
     int Row;
     int Col;
+    vector<vector<double>> data;
 
 public:
     Matrix(int row, int col){
         Row = row;
         Col = col;
+
+        data.resize(Row); // Create 'Row' number of empty vectors
+        for (int i = 0; i < Row; i++) {
+            data[i].resize(Col, 0.0); 
+    }
     }
 
+    
     int getRow(){
         return Row;
     }
@@ -19,11 +28,20 @@ public:
         return Col;
     }
 
+    void print() {
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < Col; j++) {
+                std::cout << data[i][j] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
 };
 
 int main() {
     Matrix matrix1(6, 7);
-    std::cout << matrix1.getRow() << "\n";
-    std::cout << matrix1.getCol() << "\n";
+    std::cout << "This is a " << matrix1.getRow() << "x" << matrix1.getCol() << " matrix\n";
+    matrix1.print();
     return 0;
 }
