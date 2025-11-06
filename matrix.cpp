3a8876eacb7b015b1f4da991b02b2d4fa17f86f9
ibenehasit <ibeneessem@hotmail.com>
2025-11-06 15:43:39 +0000

    added comments for readability

--- matrix.cpp
+++ matrix.cpp
@@ -100,23 +100,36 @@ public:
 };
 
 int main() {
-    Matrix matrix1(6, 7);
+    //test for empty matrix 
+    /*Matrix matrix1(6, 7);
     std::cout << "This is a " << matrix1.getRow() << "x" << matrix1.getCol() << " matrix\n";
     matrix1.print();
     
+    //test for matrix multiplication
     Matrix matrix2(7, 3);
     Matrix result1 = matrix1 * matrix2;
     std::cout << "\nMultiplication result is a " << result1.getRow() << "x" << result1.getCol() << " matrix\n";
     result1.print();
     
+
+    //test for matrix-scalar multiplication    
     Matrix result2 = matrix1 * 5;
     std::cout << "\nScalar multiplication result is a " << result2.getRow() << "x" << result2.getCol() << " matrix\n";
     result2.print();
     
+    //test for matrix addition
     Matrix matrix3(6,7);
     std::cout << "\nAddition result is a " << matrix3.getRow() << "x" << matrix3.getCol() << " matrix\n";
     Matrix result3 = matrix1 + matrix3;
     result3.print();
-    
+    */
+
+    //printing a given-value matrix
+    Matrix matrixFilled1({{1, 2, 3, 4},
+                         {5, 6, 7, 8}});
+
+    std::cout << "This is a " << matrixFilled1.getRow() << "x" << matrixFilled1.getCol() << " matrix\n";
+    matrixFilled1.print();
+
     return 0;
 }
